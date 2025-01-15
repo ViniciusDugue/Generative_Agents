99ad9a9d6149407c21fb38d06dd489a7c5b51b4f
Hunter <huntermonster8@gmail.com>
2025-01-14 17:15:07 -0800

    Implemented tick-rate for damage for agents

--- GenerativeAgentsUnityProject/Assets/Scripts/AgentHealth.cs
+++ GenerativeAgentsUnityProject/Assets/Scripts/AgentHealth.cs
@@ -7,23 +7,25 @@ public class AgentHealth : MonoBehaviour
 
     public int maxHealth = 100;
     public int currentHealth;
-
     public HealthBar healthBar;
+    private bool canTakeDamage;
 
-    // Start is called before the first frame update
+    // Initialize Health Values
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        canTakeDamage = true;
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("enemyAgent"))
+        if (collision.gameObject.CompareTag("enemyAgent") && canTakeDamage)
         {
             TakeDamage(25);
             Debug.Log("We hit an enemy. We took damage.");
             Debug.Log("Current health: " + currentHealth);
+            StartCoroutine(HitTimer(1));
         }
 
         if (currentHealth <= 0)
@@ -32,12 +34,19 @@ public class AgentHealth : MonoBehaviour
         }
     }
 
+    private IEnumerator HitTimer(float timer) {
+        canTakeDamage = false; // Disable damage
+        yield return new WaitForSeconds(timer); // Wait for the duration
+        canTakeDamage = true; // Re-enable damage
+    }
+
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
         healthBar.SetHealth(currentHealth);
     }
+
 
     void Die()
     {
