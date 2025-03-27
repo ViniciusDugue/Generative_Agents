9c45865770b37cf528cd4c53e4b13043df7adcdd
ViniciusDugue <103336607+ViniciusDugue@users.noreply.github.com>
2025-03-26 23:31:50 -0700

    Commit 6 Prep before merging main into this

--- GenerativeAgentsUnityProject/Assets/Scripts/FoodGather/FoodGathererAgent.cs
+++ GenerativeAgentsUnityProject/Assets/Scripts/FoodGather/FoodGathererAgent.cs
@@ -10,41 +10,22 @@ public class FoodGathererAgent : AgentBehavior
     public EnvironmentSettings m_EnvironmentSettings;
     public GameObject area;
     FoodSpawner m_MyArea;
-    bool m_Frozen;
-    bool m_Poisoned;
-    bool m_Satiated;
-    bool m_Shoot;
-    float m_FrozenTime;
-    float m_EffectTime;
     Rigidbody m_AgentRb;
-    float m_LaserLength;
-    // Speed of agent rotation.
-    public float turnSpeed = 300;
 
-    // Speed of agent movement.
+    // Movement settings.
+    public float turnSpeed = 300;
     public float moveSpeed = 2;
     public bool agentReset = true;
-    public Material normalMaterial;
-    public Material badMaterial;
-    public Material goodMaterial;
-    public Material frozenMaterial;
-    public GameObject myLaser;
-    public bool contribute;
-    public bool useVectorObs;
-    [Tooltip("Use only the frozen flag in vector observations. If \"Use Vector Obs\" " +
-             "is checked, this option has no effect. This option is necessary for the " +
-             "VisualFoodCollector scene.")]
-    public bool useVectorFrozenFlag;
-    
+
     public TextMeshProUGUI fitnessScoreText; 
     public int agentIndex; 
     private float fitnessScore = 0f;
+    public bool useVectorObs;
 
     EnvironmentParameters m_ResetParams;
 
     public override void Initialize()
     {
-        // exhaustionRate = 2.0f;
         m_AgentRb = GetComponent<Rigidbody>();
         m_MyArea = area.GetComponent<FoodSpawner>();
         m_ResetParams = Academy.Instance.EnvironmentParameters;
@@ -58,145 +39,41 @@ public class FoodGathererAgent : AgentBehavior
         {
             var localVelocity = transform.InverseTransformDirection(m_AgentRb.velocity);
             sensor.AddObservation(localVelocity);
-            // sensor.AddObservation(m_Frozen);
-            // sensor.AddObservation(m_Shoot);
         }
-        // else if (useVectorFrozenFlag)
-        // {
-        //     sensor.AddObservation(m_Frozen);
-        // }
-    }
-
-    public Color32 ToColor(int hexVal)
-    {
-        var r = (byte)((hexVal >> 16) & 0xFF);
-        var g = (byte)((hexVal >> 8) & 0xFF);
-        var b = (byte)(hexVal & 0xFF);
-        return new Color32(r, g, b, 255);
-    }
-
-    public void MoveAgent(ActionBuffers actionBuffers)
-    {
-        m_Shoot = false;
-
-        if (Time.time > m_FrozenTime + 4f && m_Frozen)
-        {
-            Unfreeze();
-        }
-        if (Time.time > m_EffectTime + 0.5f)
-        {
-            if (m_Poisoned)
-            {
-                Unpoison();
-            }
-            if (m_Satiated)
-            {
-                Unsatiate();
-            }
-        }
-
-        var dirToGo = Vector3.zero;
-        var rotateDir = Vector3.zero;
-
-        var continuousActions = actionBuffers.ContinuousActions;
-        var discreteActions = actionBuffers.DiscreteActions;
-
-        if (!m_Frozen)
-        {
-            var forward = Mathf.Clamp(continuousActions[0], -1f, 1f);
-            var right = Mathf.Clamp(continuousActions[1], -1f, 1f);
-            var rotate = Mathf.Clamp(continuousActions[2], -1f, 1f);
-
-            dirToGo = transform.forward * forward;
-            dirToGo += transform.right * right;
-            rotateDir = -transform.up * rotate;
-
-            var shootCommand = discreteActions[0] > 0;
-            if (shootCommand)
-            {
-                m_Shoot = true;
-                dirToGo *= 0.5f;
-                m_AgentRb.velocity *= 0.75f;
-            }
-            m_AgentRb.AddForce(dirToGo * moveSpeed, ForceMode.VelocityChange);
-            transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
-        }
-
-        if (m_AgentRb.velocity.sqrMagnitude > 25f) // slow it down
-        {
-            m_AgentRb.velocity *= 0.95f;
-        }
-
-        if (m_Shoot)
-        {
-            var myTransform = transform;
-            myLaser.transform.localScale = new Vector3(1f, 1f, m_LaserLength);
-            var rayDir = 25.0f * myTransform.forward;
-            Debug.DrawRay(myTransform.position, rayDir, Color.red, 0f, true);
-            RaycastHit hit;
-            if (Physics.SphereCast(transform.position, 2f, rayDir, out hit, 25f))
-            {
-                if (hit.collider.gameObject.CompareTag("agent"))
-                {
-                    hit.collider.gameObject.GetComponent<FoodGathererAgent>().Freeze();
-                }
-            }
-        }
-        else
-        {
-            myLaser.transform.localScale = new Vector3(0f, 0f, 0f);
-        }
-    }
-
-    void Freeze()
-    {
-        gameObject.tag = "frozenAgent";
-        m_Frozen = true;
-        m_FrozenTime = Time.time;
-        //gameObject.GetComponentInChildren<Renderer>().material = frozenMaterial;
-    }
-
-    void Unfreeze()
-    {
-        m_Frozen = false;
-        gameObject.tag = "agent";
-        //gameObject.GetComponentInChildren<Renderer>().material = normalMaterial;
-    }
-
-    void Poison()
-    {
-        m_Poisoned = true;
-        m_EffectTime = Time.time;
-        //gameObject.GetComponentInChildren<Renderer>().material = badMaterial;
-    }
-
-    void Unpoison()
-    {
-        m_Poisoned = false;
-        //gameObject.GetComponentInChildren<Renderer>().material = normalMaterial;
-    }
-
-    void Satiate()
-    {
-        m_Satiated = true;
-        m_EffectTime = Time.time;
-        //gameObject.GetComponentInChildren<Renderer>().material = goodMaterial;
-    }
-
-    void Unsatiate()
-    {
-        m_Satiated = false;
-        //gameObject.GetComponentInChildren<Renderer>().material = normalMaterial;
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
-
     {
         MoveAgent(actionBuffers);
     }
 
+    public void MoveAgent(ActionBuffers actionBuffers)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var continuousActions = actionBuffers.ContinuousActions;
+
+        // move forward/backward and rotation handling
+        var forward = Mathf.Clamp(continuousActions[0], -1f, 1f);
+        var rotate = Mathf.Clamp(continuousActions[2], -1f, 1f);
+
+        dirToGo = transform.forward * forward;
+        rotateDir = -transform.up * rotate;
+
+        m_AgentRb.AddForce(dirToGo * moveSpeed, ForceMode.VelocityChange);
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
+
+        // prevent speed from increasing too much
+        if (m_AgentRb.velocity.sqrMagnitude > 25f)
+        {
+            m_AgentRb.velocity *= 0.95f;
+        }
+    }
+
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        //movement heuristics
         var continuousActionsOut = actionsOut.ContinuousActions;
         if (Input.GetKey(KeyCode.A))
         {
@@ -214,29 +91,25 @@ public class FoodGathererAgent : AgentBehavior
         {
             continuousActionsOut[0] = -1;
         }
-        var discreteActionsOut = actionsOut.DiscreteActions;
-        discreteActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
     }
 
     public override void OnEpisodeBegin()
     {   
-        if(agentReset) { 
-            Unfreeze();
-            Unpoison();
-            Unsatiate();
-            m_Shoot = false;
+        if (agentReset)
+        { 
             m_AgentRb.velocity = Vector3.zero;
-            myLaser.transform.localScale = new Vector3(0f, 0f, 0f);
-            transform.position = new Vector3(Random.Range(-m_MyArea.range, m_MyArea.range),
-                2f, Random.Range(-m_MyArea.range, m_MyArea.range))
-                + area.transform.position;
+            // respawn agent randomly
+            transform.position = new Vector3(
+                Random.Range(-m_MyArea.range, m_MyArea.range),
+                2f,
+                Random.Range(-m_MyArea.range, m_MyArea.range)
+            ) + area.transform.position;
             transform.rotation = Quaternion.Euler(new Vector3(0f, Random.Range(0, 360)));
 
             SetResetParameters();
             Debug.Log("Episode complete.");
 
-            // Only reset the environment if Agent is training
-        
+            // reset environment if training
             m_EnvironmentSettings.EnvironmentReset();
         }
     }
@@ -253,52 +126,28 @@ public class FoodGathererAgent : AgentBehavior
     {
         if (collision.gameObject.CompareTag("food"))
         {
-            Debug.Log("food collision");
-            if (collision.gameObject.GetComponent<FoodScript>() == null)
+            Debug.Log("Food collision");
+            var foodScript = collision.gameObject.GetComponent<FoodScript>();
+            if (foodScript == null)
             {
                 Debug.LogError("FoodScript is null on collided object!");
             }
-            Satiate();
-            collision.gameObject.GetComponent<FoodScript>().OnEaten();
-            AddReward(1f);
-            fitnessScore += 1f;
-            UpdateFitnessScoreText();
-            if (contribute)
+            else
             {
-                Debug.Log("Contributing to foodScore");
-                m_EnvironmentSettings.foodScore += 1;
+                foodScript.OnEaten();
+                AddReward(1f);
+                fitnessScore += 1f;
+                UpdateFitnessScoreText();
+
+                if (m_EnvironmentSettings != null)
+                {
+                    m_EnvironmentSettings.foodScore += 1;
+                }
             }
         }
-        if (collision.gameObject.CompareTag("badFood"))
-        {
-            Poison();
-            collision.gameObject.GetComponent<FoodScript>().OnEaten();
-
-            AddReward(-1f);
-            if (contribute)
-            {
-                Debug.Log("Contributing to foodScore");
-                m_EnvironmentSettings.foodScore -= 1;
-            }
-        }
-
-    }
-
-    public void SetLaserLengths()
-    {
-        m_LaserLength = m_ResetParams.GetWithDefault("laser_length", 1.0f);
-    }
-
-    public void SetAgentScale()
-    {
-        float agentScale = m_ResetParams.GetWithDefault("agent_scale", 1.0f);
-        gameObject.transform.localScale = new Vector3(agentScale, agentScale, agentScale);
     }
 
     public void SetResetParameters()
     {
-        SetLaserLengths();
-        SetAgentScale();
     }
-
 }
