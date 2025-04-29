129754c33968b0d176fca37c4c392d162eeb7bb9
ViniciusDugue <103336607+ViniciusDugue@users.noreply.github.com>
2025-04-29 03:01:01 -0700

    Resolved Minor Conflicts

--- GenerativeAgentsUnityProject/Assets/Scripts/Build Behavior.cs
+++ GenerativeAgentsUnityProject/Assets/Scripts/Build Behavior.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:GenerativeAgentsUnityProject/Assets/Scripts/Build Behavior.cs
 public class BuildBehavior : MonoBehaviour
-========
-public class MapManager : MonoBehaviour
->>>>>>>> main:GenerativeAgentsUnityProject/Assets/Scripts/Map/MapManager.cs
 {
     // Start is called before the first frame update
     void Start()
